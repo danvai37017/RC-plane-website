40445650d620f00fb152164c628288a6ef3760ad
Dan <dvaidhyan@gmail.com>
2026-07-07 16:52:28 -0700

    Hid IP addresses v2

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 @echo off
 call npm run build
 for /f "tokens=1,2 delims==" %%I in (.env) do set %%I=%%J
-scp -r -i "C:\Users\Daniel\Downloads\ssh-key-2026-07-07.key" ./dist/. ubuntu@64.181.238.57:/var/www/html/
+scp -r -i "%KEY_PATH%" ./dist/. ubuntu@%SERVER_IP%:/var/www/html/
 pause
