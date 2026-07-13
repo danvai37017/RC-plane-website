57716772b55d63e910faf33fac8bc695dc85eb47
Dan <dvaidhyan@gmail.com>
2026-07-13 15:41:17 -0700

    First Iteration

--- deploy.bat
+++ deploy.bat
@@ -2,4 +2,5 @@
 call npm run build
 for /f "tokens=1,2 delims==" %%I in (.env) do set %%I=%%J
 scp -r -i "%KEY_PATH%" ./dist/. ubuntu@%SERVER_IP%:/var/www/html/
+ssh -i "%KEY_PATH%" ubuntu@%SERVER_IP% "sudo /usr/local/bin/fix-web-perms.sh"
 pause
